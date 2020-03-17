b2b16de5615d6de3ab5d108ab3c51373d00217bf
Jennifer Gamez <jenggamez@gmail.com>
2020-03-17 15:14:34 -0700

    initial commit

---
+++ precache-manifest.ea1537b0e8ccb88e1ce35fbd0db2b828.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "465fa0b22a845c1dff9e79770ab8bec2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "500cb676532d4d9e6fda",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "18d0697844c6e707ded2",
+    "url": "/static/css/main.f2324500.chunk.css"
+  },
+  {
+    "revision": "500cb676532d4d9e6fda",
+    "url": "/static/js/2.6d4f3c41.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.6d4f3c41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18d0697844c6e707ded2",
+    "url": "/static/js/main.fa9dc011.chunk.js"
+  },
+  {
+    "revision": "eb629388abded9468c90",
+    "url": "/static/js/runtime-main.c1b8bb11.js"
+  },
+  {
+    "revision": "a34eb125129c71680f1672e19018311d",
+    "url": "/static/media/Codingquiz.a34eb125.png"
+  },
+  {
+    "revision": "1b7f506e184bbf79355dd5b4a264064b",
+    "url": "/static/media/JenGamez.1b7f506e.jpg"
+  },
+  {
+    "revision": "16288a8f94be27e3aa2827dce4c3a8ff",
+    "url": "/static/media/Shotcaller.16288a8f.png"
+  },
+  {
+    "revision": "580e13ad4c2e19b8f7fcbb5b5d7128a3",
+    "url": "/static/media/moviememe.580e13ad.png"
+  },
+  {
+    "revision": "943bc1782cf831c2510f36067337a6b3",
+    "url": "/static/media/resume.943bc178.pdf"
+  },
+  {
+    "revision": "2c471fc61492e7fee4fcdac14bc75c0a",
+    "url": "/static/media/weatherdashboard.2c471fc6.png"
+  }
+]);
